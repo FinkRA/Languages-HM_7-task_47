e98622245ee24829cf8f26063f370c1432937e82
Rudolf Fink <rudolf.baldr.fink@gmail.com>
2022-12-18 02:08:27 +0500

    done

--- Program.cs
+++ Program.cs
@@ -3,3 +3,25 @@
 // 0,5      7       -2      -0,2
 // 1        -3,3    8       -9,9
 // 8        7,8     -7,1    9
+
+// Я РЕШИЛ, ЧТО С ОГРАНИЧЕНИЕМ РАНДОМА ДО 10 и ОКРУГЕНИЕМ БУДЕТ ПОСИМПОТИЧНЕЕ
+
+Console.Write("Ведите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m, n];
+var element = new Random();
+
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = element.NextDouble() * 10;
+        array[i, j] = Math.Round(array[i, j], 2);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
